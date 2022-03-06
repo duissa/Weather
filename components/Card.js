853765fb382b7f0b2b5358ae7e63135a6b366188
duissa <duissa@gmail.com>
2022-03-06 22:00:38 +0200

    first

--- components/Card.js
+++ components/Card.js
@@ -19,8 +19,8 @@ export const Card = (props) => {
       <div className={styles.container}>
         <div><h1>{props.message.location}</h1></div>
         <div ><img className={styles.image} src={`http://openweathermap.org/img/w/${props.message.icon}.png`} alt="" /></div>
-        <div><h2>{props.message.temperature}</h2></div>
-        <div><h3>Feels Like: {props.message.feelsLike} </h3></div>
+        <div><h2>{props.message.temperature}°C</h2></div>
+        <div><h3>Feels Like: {props.message.feelsLike}°C </h3></div>
     
         
       </div>
